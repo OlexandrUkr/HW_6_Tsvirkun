7a3f8e10c189d5b8feebc5ddefd6d72b62fb7f73
Olexandr <olexandrcv@gmail.com>
2023-01-28 21:13:08 +0200

    Correction of remarks

--- Task_1/script_1.py
+++ Task_1/script_1.py
@@ -1,24 +1,43 @@
 class IteratorFib:
 
     def __init__(self, nmax):
+        self.n1 = 0
+        self.n2 = 1
         self.max = nmax
 
     def __iter__(self):
-        self.n1 = 0
-        self.n2 = 1
         return self
 
     def __next__(self):
-        current = self.n1
-        if current > self.max:
+        self.current = self.n1
+        if self.current > self.max:
             raise StopIteration
         self.n0 = self.n1
         self.n1 = self.n2
         self.n2 = self.n0 + self.n2
-        if current % 2 == 0:
-            return f"{current} - парне число"
-        return current
+        return self.current
+
+    def is_odd(self):
+        if self.current % 2 == 0:
+            return "Число парне"
+        else:
+            return "Число непарне"
 
 
-for i in IteratorFib(255):
+"""
+for i in IteratorFib(20):
     print(i)
+"""
+fib = IteratorFib(255)
+print(next(fib))
+print(next(fib))
+print(fib.is_odd())
+print(next(fib))
+print(next(fib))
+print(next(fib))
+print(fib.is_odd())
+print(next(fib))
+print(next(fib))
+print(fib.is_odd())
+print(next(fib))
+print(next(fib))
